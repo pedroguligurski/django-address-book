c616d787c5ff4b9a1f021131cdb353134a2f5248
guligurski-alfama <pedro.guligurski@alfama.com.br>
2025-05-30 13:31:54 -0300

    Add Contact model and admin registration; update settings for localization

--- project/settings.py
+++ project/settings.py
@@ -105,11 +105,11 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
-USE_I18N = True
+USE_I18N = True 
 
 USE_TZ = True
 
